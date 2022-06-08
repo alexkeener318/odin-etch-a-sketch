fe6bef7d87abc16e12d3b4e542be26d5ef3b0c79
Alex Keener <alexkeener318@gmail.com>
2022-06-08 16:24:35 -0500

    Sets up basic coloring of grid

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let container = document.querySelector(".container");
 // creates the 16x16 divs
-let counter = 1;
 for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         const temp = document.createElement('div');
         temp.classList.add("grid");
-        temp.textContent = "TEST " + counter;
-        counter++;
+        temp.textContent = "TEST ";
         container.appendChild(temp);
     }
 }
+
+function changeColor(e) {
+    console.log(e.target);
+    const div = e.target;
+    div.style.backgroundColor = "black";
+}
+
+let width = 800 / 16;
+let grid = document.querySelectorAll(".grid");
+console.log(grid);
+grid.forEach(square => {
+    square.addEventListener('mouseenter', changeColor);
+});
